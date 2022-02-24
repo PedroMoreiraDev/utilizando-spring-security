661008c0a002571c533ee92f80861aa7fc6250ed
PedroMoreiraDev <pedrodev2020@gmail.com>
2022-02-23 21:39:00 -0300

    enviando implementacao de user security

---
+++ user-login-emai-lverification/src/main/java/com/example/user/login/emai/lverification/appUser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.example.user.login.emai.lverification.appUser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ user-login-emai-lverification/src/test/java/com/example/user/login/emai/lverification/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user.login.emai.lverification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
